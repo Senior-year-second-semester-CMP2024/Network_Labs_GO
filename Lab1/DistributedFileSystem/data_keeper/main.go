fd1617303495e21da7e803b4d4d9f89243953e63
bemoierian <bemoi.erian@gmail.com>
2024-03-11 18:03:07 +0200

    added upload file

--- Lab1/DistributedFileSystem/data_keeper/main.go
+++ Lab1/DistributedFileSystem/data_keeper/main.go
@@ -1,19 +1,40 @@
 package main
 
 import (
-	"fmt"
-	// "context"
-	// "net"
-	// "strings"
+	"context"
+	"log"
+	"net"
+	"os"
+
+	pb "DistributedFileSystem/dfs" // Import the generated Go code
+
 	"google.golang.org/grpc"
 )
 
-func main() {
-	conn, err := grpc.Dial("localhost:8080", grpc.WithInsecure())
+type server struct {
+	pb.UnimplementedDFSServer
+}
+
+func (s *server) UploadFile(ctx context.Context, req *pb.UploadFileRequest) (*pb.UploadFileResponse, error) {
+	// Save the uploaded file to a folder
+	err := os.WriteFile(req.FileName, req.FileData, 0644)
 	if err != nil {
-		fmt.Println("did not connect:", err)
-		return
+		log.Println("Failed to save file:", err)
+		return nil, err
 	}
-	defer conn.Close()
-	fmt.Println("Hello, world!")
+	log.Println("File saved successfully:", req.FileName)
+	return &pb.UploadFileResponse{Message: "File uploaded successfully"}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":50051")
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterDFSServer(s, &server{})
+	log.Println("Server started at :50051")
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
